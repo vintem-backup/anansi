486735e7f30c720fb1927a06fcc8622cd750ad93
marcusmello <mandealista@gmail.com>
2020-08-27 18:52:29 -0300

    Practicing with 'CustomerMixin' | Almost finished 'TraderMixin'

--- anansi/tradingbot/mixins.py
+++ anansi/tradingbot/mixins.py
@@ -1,5 +1,9 @@
-class CustomerMixin:
-    pass
+from marketdata import klines
+
+
+class CustomerMixin(object):
+    def get_name(self):
+        return "{}".format(self.user_name)
 
 
 class PositionMixin:
@@ -7,16 +11,47 @@ class PositionMixin:
 
 
 class TraderMixin:
-    pass
 
+    def _init(self):
+        self.Classifier = getattr(
+            classifiers, self.classifier_name)(
+                self.classifier_parameters)
 
-# class BkpTrader:
-#    def __init__(self, market=Default.market, classifier=Default.classifier,
-#                 stop_loss=Default.stop_loss, mode=Default.mode):
-#        self.market = market
-#        self.classifier = classifier
-#        self.stop_loss = stop_loss
-#        self.mode = mode
+        if self.mode == "BackTesting":
+            # self._now =
+            self.KStreamer = klines.FromBroker(
+                broker_name=self.exchange, symbol=self.symbol)
 
-#    def convert_time_frame(self, tf):  # Just an example
-#        return tools.ConvertTimeFrame(tf).to_seconds()
+    def _do_analysis(self):
+        positioned = bool(self.position.side != "zeroed")
+        if not self.by_pass_stop:
+            if positioned:
+                self.stop_analysis()
+        self.classifier_analysis()
+
+    def _analysis_pipeline(self, Analyzer):
+        self.KStreamer.time_frame = self.Analyzer.parameters.time_frame
+        self.Analyzer.klines = KStreamer.newest(
+            number_of_candles=Analyzer.how_many_candles()
+        )
+        self.Analyzer.proceed_analysis()
+
+    def classifier_analysis(self):
+        self._analysis_pipeline(Classifier)
+
+    def stop_analysis(self):
+        self._analysis_pipeline(self.StopLoss)
+
+    def _step_forward_cycle(self):
+        pass
+
+    def _end(self):
+        pass
+
+    def run(self):
+        self._init()
+        while self.status == "Running":
+            self._do_analysis()
+            self._step_forward_cycle()
+
+        self._end()
