9bf8a1f0e55e657725868d4ca199969315a44997
marcusmello <mandealista@gmail.com>
2020-09-07 20:46:09 -0300

    Some improvement, starting from the older 'brokers' module

--- anansi/marketdata/data_brokers.py
+++ anansi/marketdata/data_brokers.py
@@ -1,20 +1,97 @@
-import sys
-try:
-    sys.path.insert(0, '..')
-    import settings
-    import my_tools
-
-except Exception as e:
-    print(e)
-
+from .. import settings
+from ..share.tools import (
+    DocInherit as doc_inherit,
+    ConvertTimeFrame as ParseTime)
 import pandas as pd
 import requests
 import pendulum
 
 
-class Broker:
-    def __init__(self):
-        pass
+def wrapper_for(broker_name: str) -> str:
+    _brokers = {
+        "binance": "BinanceDataWrapper",
+    }
+    return _brokers[broker_name.lower()]
+
+
+def get_response(endpoint: str) -> requests.models.Response:
+    with requests.get(endpoint) as response:
+        if response.status_code == 200:
+            return response
+
+
+def remove_last_if_unclosed(klines):
+    last_open_time = klines[-1:].Open_time.item()
+    delta_time = (pendulum.now(tz="UTC")).int_timestamp - last_open_time
+    unclosed = bool(delta_time < klines.attrs["SecondsTimeFrame"])
+
+    if unclosed:
+        return klines[:-1]
+    return klines
+
+
+class KlinesPreFormat:
+    __slots__ = [
+        "time_frame",
+        "DateTimeFmt",
+        "DateTimeUnit",
+        "columns",
+        "formatted_klines",
+    ]
+
+    def __init__(
+        self, time_frame,
+        klines: list,
+        DateTimeFmt: str,
+        DateTimeUnit: str,
+        columns: list
+    ):
+
+        self.time_frame = time_frame
+        self.DateTimeFmt = DateTimeFmt
+        self.DateTimeUnit = DateTimeUnit
+        self.columns = columns
+        self.formatted_klines = [self.format_each(kline) for kline in klines]
+
+    def format_datetime(self, datetime_in, truncate_seconds_to_zero=False) -> int:
+        if self.DateTimeFmt == "timestamp":
+            if self.DateTimeUnit == "seconds":
+                datetime_out = int(float(datetime_in))
+            elif self.DateTimeUnit == "milliseconds":
+                datetime_out = int(float(datetime_in) / 1000)
+
+            if truncate_seconds_to_zero:
+                _date_time = pendulum.from_timestamp(int(datetime_out))
+                if _date_time.second != 0:
+                    datetime_out = (
+                        _date_time.subtract(seconds=_date_time.second)
+                    ).int_timestamp
+
+        return datetime_out
+
+    def format_each(self, kline: list) -> list:
+
+        return [
+            self.format_datetime(_item, truncate_seconds_to_zero=True)
+            if kline.index(_item) == self.columns.index("Open_time")
+            else self.format_datetime(_item)
+            if kline.index(_item) == self.columns.index("Close_time")
+            else float(_item)
+            for _item in kline
+        ]
+
+    def to_dataframe(self) -> pd.DataFrame:
+        klines = pd.DataFrame(
+            self.formatted_klines,
+            columns=self.columns
+        ).astype({"Open_time": "int32", "Close_time": "int32"})
+
+        klines.attrs.update(
+            {"SecondsTimeFrame": ParseTime(self.time_frame).to_seconds()})
+        return klines
+
+
+class DataBroker:
 
     def server_time(self) -> int:
         """Data e horário do servidor da corretora
@@ -48,8 +125,8 @@ class Broker:
         Args:
             symbol (str): Símbolo do ativo
             time_frame (str): Intervalo (escala) dos candlesticks
-            ignore_opened_candle (bool, optional): [description]. Defaults to True.
-            show_only_desired_info (bool, optional): [description]. Defaults to True.
+            ignore_opened_candle (bool, optional): Defaults to True
+            show_only_desired_info (bool, optional): Defaults to True.
 
         **kwargs:
             number_of_candles (int): Número de candelesticks
@@ -82,75 +159,10 @@ class Broker:
         raise NotImplementedError
 
 
-doc_inherit = my_tools.DocInherit
-
-
-def wrapper_for(broker_name: str) -> str:
-    _brokers = {
-        "binance": "BinanceApiWrapper",
-    }
-    return _brokers[broker_name.lower()]
-
-
-def get_response(endpoint: str) -> requests.models.Response:
-    with requests.get(endpoint) as response:
-        if response.status_code == 200:
-            return response
-
-
-class Transform:
-    def __init__(self,
-                 klines: list,
-                 DateTimeFmt: str,
-                 DateTimeUnit: str,
-                 columns: list):
-
-        self.DateTimeFmt = DateTimeFmt
-        self.DateTimeUnit = DateTimeUnit
-        self.columns = columns
-        self.formatted_klines = [self.format_each(kline)
-                                 for kline in klines]
-
-    def format_datetime(
-            self,
-            datetime_in,
-            truncate_seconds_to_zero=False) -> int:
-        if self.DateTimeFmt == 'timestamp':
-            if self.DateTimeUnit == 'seconds':
-                datetime_out = int(float(datetime_in))
-            elif self.DateTimeUnit == 'milliseconds':
-                datetime_out = int(float(datetime_in)/1000)
-
-            if truncate_seconds_to_zero:
-                _date_time = pendulum.from_timestamp(int(datetime_out))
-                if _date_time.second != 0:
-                    datetime_out = (_date_time.subtract(
-                        seconds=_date_time.second)).int_timestamp
-
-        return datetime_out
-
-    def format_each(self, kline: list) -> list:
-
-        return [
-            self.format_datetime(_item, truncate_seconds_to_zero=True)
-            if kline.index(_item) == self.columns.index("Open_time")
-            else self.format_datetime(_item)
-            if kline.index(_item) == self.columns.index("Close_time")
-            else float(_item)
-            for _item in kline
-        ]
-
-    def to_dataframe(self) -> pd.DataFrame:
-        return (
-            pd.DataFrame(
-                self.formatted_klines, columns=self.columns
-            ).astype({'Open_time': 'int32', 'Close_time': 'int32'}))
-
-
-class BinanceApiWrapper(Broker, settings.Binance_):
+class BinanceDataWrapper(DataBroker, settings.Binance_):
     @doc_inherit
     def __init__(self):
-        super(BinanceApiWrapper, self).__init__()
+        super(BinanceDataWrapper, self).__init__()
 
     @doc_inherit
     def server_time(self) -> int:
@@ -190,22 +202,24 @@ class BinanceApiWrapper(Broker, settings.Binance_):
             endpoint += "&endTime={}".format(str(until * 1000))
         if number_of_candles:
             endpoint += "&limit={}".format(str(number_of_candles))
-        _klines = (get_response(endpoint)).json()
+        raw_klines = (get_response(endpoint)).json()
+
+        klines = KlinesPreFormat(
+            time_frame,
+            raw_klines,
+            DateTimeFmt=self.DateTimeFmt,
+            DateTimeUnit=self.DateTimeUnit,
+            columns=self.kline_informations,
+        ).to_dataframe()
+
         if ignore_opened_candle:
-            now = (pendulum.now(tz="UTC")).int_timestamp
-            last_open_time = int((float(_klines[-1][0])) / 1000)
-            if ((now - last_open_time)
-                    < settings.TimeFrames().seconds_in(time_frame)):
-                _klines = _klines[:-1]
+            klines = remove_last_if_unclosed(klines)
 
-        klines_object = Transform(_klines, DateTimeFmt=self.DateTimeFmt,
-                                  DateTimeUnit=self.DateTimeUnit,
-                                  columns=self.kline_information_map).to_dataframe()
         if show_only_desired_info:
-            return klines_object[settings.klines_desired_info]
-        return klines_object
+            return klines[settings.kline_desired_informations]
+        return klines
 
-    @ doc_inherit
+    @doc_inherit
     def _oldest_open_time(self, symbol: str, time_frame: str) -> int:
         return self.klines(
             symbol=symbol, time_frame=time_frame, since=1, number_of_candles=1
