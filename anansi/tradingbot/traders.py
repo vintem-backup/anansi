9697881976b40008edac6ba59d6254c73f4ec3b0
marcusmello <mandealista@gmail.com>
2020-09-12 19:28:13 -0300

    Some new methods

--- anansi/tradingbot/traders.py
+++ anansi/tradingbot/traders.py
@@ -49,8 +49,35 @@ class DefaultTrader:
         self.OrderHandler = OrderHandler(operation=self.operation)
 
         if self.operation.mode == mode.BackTesting:
-
-            self.klines.time_frame = (
-                self.Classifier.parameters.time_frame)
-
+            self.klines.time_frame = self.Classifier.parameters.time_frame
             self._now = self.klines._oldest_open_time()
+            self.operation.position.update_current_side_to(side.Zeroed)
+
+    def run(self):
+        while self.operation.status == stat.Running:
+            self._do_analysis()
+            self._loop()
+
+        self._end()
+
+    def _loop(self):
+        if self.operation.mode == mode.BackTesting:
+            self._now += self._step
+        else:
+            time.sleep(self._step)
+            self._now = pendulum.now().int_timestamp
+
+    def _do_analysis(self):
+        if (not self.operation.ignore_stop_loss) and (
+                self.operation.position.current_side != side.Zeroed):
+            # self._step =
+            self.stop_analysis()
+        else:
+            # self._step =
+        self.classifier_analysis()
+
+    def stop_analysis(self):
+        pass
+
+    def classifier_analysis(self):
+        pass
