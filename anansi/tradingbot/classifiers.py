1b55a3a3b8bc3419d23657eb74036ac29237d590
marcusmello <mandealista@gmail.com>
2020-09-12 19:28:07 -0300

    Default parameters, made correctly

--- anansi/tradingbot/classifiers.py
+++ anansi/tradingbot/classifiers.py
@@ -1,6 +1,26 @@
+import json
+from ..share import tools
+convert = tools.ConvertTimeFrame
+deserialize = tools.Deserialize()
+
+
 class CrossSMA:
+
+    class DefaultParameters:
+        def __init__(self):
+
+            self.time_frame = "6h"
+            self.price_source = "ohlc4"
+            self.smaller_sample = 3
+            self.larger_sample = 80
+
     def __init__(self, operation):
         self.operation = operation
 
-    class parameters:
-        time_frame = "12h"
+        if not self.operation.classifier_parameters:
+            self.operation.update_classifier_parameters_to(
+                json.dumps(self.DefaultParameters().__dict__)
+            )
+
+        self.parameters = deserialize.json2obj(
+            self.operation.classifier_parameters)
