bade0a04c5d2e8b62ed7a46e5333a1afbaa30c1b
marcusmello <mandealista@gmail.com>
2020-09-13 14:55:25 -0300

    New 'how_many_candles()' | New json.dumps parameters

--- anansi/tradingbot/classifiers.py
+++ anansi/tradingbot/classifiers.py
@@ -1,14 +1,14 @@
 import json
 from ..share import tools
-convert = tools.ConvertTimeFrame
+#convert = tools.ConvertTimeFrame
 deserialize = tools.Deserialize()
 
 
 class CrossSMA:
-
     class DefaultParameters:
         def __init__(self):
-
+            # TODO: Create classes 'PossiblePriceSource' and
+            # 'PossibleTimeFrames' on settings module
             self.time_frame = "6h"
             self.price_source = "ohlc4"
             self.smaller_sample = 3
@@ -19,8 +19,12 @@ class CrossSMA:
 
         if not self.operation.classifier_parameters:
             self.operation.update_classifier_parameters_to(
-                json.dumps(self.DefaultParameters().__dict__)
+                json.dumps(self.DefaultParameters(),
+                           default=lambda o: o.__dict__, indent=4)
             )
 
         self.parameters = deserialize.json2obj(
             self.operation.classifier_parameters)
+
+    def how_many_candles(self) -> int:
+        return self.parameters.larger_sample
