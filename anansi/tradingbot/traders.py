e9224bcea30bd1c8003918790b58ce7745321d25
marcusmello <mandealista@gmail.com>
2020-09-20 18:38:13 -0300

    New '_signal_interpreter' method and some attributes refactoring

--- anansi/tradingbot/traders.py
+++ anansi/tradingbot/traders.py
@@ -1,6 +1,4 @@
 import pendulum
-import pandas as pd
-from .order_handler import *
 from .models import Logger
 from . import classifiers, stop_handlers, order_handler
 from ..marketdata import klines
@@ -9,6 +7,7 @@ from ..settings import (
     PossibleSides as side,
     PossibleStatuses as stat,
     PossibleModes as mode,
+    PossibleSignals as sig,
 )
 
 
@@ -17,7 +16,8 @@ class DefaultTrader:
         self.operation = operation
         self.logger = Logger(operation=self.operation)
         self._step = None
-        self._hint_side = None
+        self._by_stop = None
+        self._signal = None
 
         self.Classifier = getattr(classifiers, self.operation.classifier.name)(
             parameters=self.operation.classifier.parameters
@@ -38,8 +38,8 @@ class DefaultTrader:
         self.KlinesGetter = klines.FromBroker(  # Por hora, evocando da corretora
             broker_name=self.operation.market.exchange,
             symbol=(
-                self.operation.market.ticker_symbol
-                + self.operation.market.quote_symbol)
+                self.operation.market.quote_asset_symbol
+                + self.operation.market.base_asset_symbol)
         )
 
         self._now = (
@@ -62,6 +62,11 @@ class DefaultTrader:
     def _get_ready_to_repeat(self):
         if self.operation.mode == mode.BackTesting:
             self._now += self._step
+            #! When the backtesting klines start to be produced
+            # synthetically from the database, this parameter must be the
+            # Open_time of the last available candle.
+            if self._now > pendulum.now().int_timestamp:
+                self.operation.status = stat.NotRunning
         else:
             time.sleep(self._step)
             self._now = pendulum.now().int_timestamp
@@ -80,10 +85,12 @@ class DefaultTrader:
         self._classifier_analysis()
 
     def _stop_analysis(self):
+        self._by_stop = True
         print("Finally stop alalysis! But, nothing here :/")
 
     def _classifier_analysis(self):
         # TODO: Acresentar "trava" de último candle analisado
+        self._by_stop = False
         self._analyze_for(self.Classifier)
 
         self.logger.results_from = (
@@ -98,8 +105,26 @@ class DefaultTrader:
             number_of_candles=Analyzer.n_samples_to_analyze, until=self._now))
 
         self.logger.last_analyzed_data = Analyzer.data_to_analyze[-1:]
-
         self.logger.analysis_result = self.analysis_result = Analyzer.result()
+        self._signal_interpreter()
+        self.OrderHandler.execute(self._signal)
+
+    def _end(self):
+        print("It's the end!")
+
+    def _signal_interpreter(self):
+        signal = order_handler.Signal(from_side=self.operation.position.side,
+                                      to_side=self.analysis_result["Hint_side"],
+                                      by_stop=self._by_stop).get()
+
+        self._signal = (sig.Hold
+                        if signal == sig.NakedSell
+                        else sig.Sell if signal == sig.DoubleNakedSell
+                        else sig.Buy if signal == sig.DoubleBuy
+                        else signal)
+
+        print(" ")
+        print("Original_signal: ", signal)
 
     def run(self):
         self.operation.update(status=stat.Running)
@@ -108,4 +133,4 @@ class DefaultTrader:
             self._do_analysis()
             self._get_ready_to_repeat()
 
-        # self._end()
+        self._end()
