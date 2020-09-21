8efa570c1455bf0e156eee1c67f4f29b9f453e47
marcusmello <mandealista@gmail.com>
2020-09-21 10:24:28 -0300

    Black formatting | New '_start' method in order to set attributes to initial values

--- anansi/tradingbot/traders.py
+++ anansi/tradingbot/traders.py
@@ -24,20 +24,23 @@ class DefaultTrader:
         )
 
         self._classifier_time_frame_in_seconds = seconds_in(
-            self.Classifier.parameters.time_frame)
+            self.Classifier.parameters.time_frame
+        )
 
         self.StopLoss = getattr(stop_handlers, self.operation.stop_loss.name)(
             parameters=self.operation.stop_loss.parameters
         )
 
         self._stop_loss_time_frame_in_seconds = seconds_in(
-            self.StopLoss.parameters.time_frame)
+            self.StopLoss.parameters.time_frame
+        )
 
         self.KlinesGetter = klines.FromBroker(  # Por hora, evocando da corretora
             broker_name=self.operation.market.exchange,
             symbol=(
                 self.operation.market.quote_asset_symbol
-                + self.operation.market.base_asset_symbol)
+                + self.operation.market.base_asset_symbol
+            ),
         )
 
         self._now = (
@@ -61,8 +64,8 @@ class DefaultTrader:
         if self.operation.mode == mode.BackTesting:
             self._now += self._step
             #! When the backtesting klines start to be produced
-            # synthetically from the database, this parameter must be the
-            # Open_time of the last available candle.
+            #! synthetically from the database, this parameter must be the
+            #! Open_time of the last available candle.
             if self._now > pendulum.now().int_timestamp:
                 self.operation.status = stat.NotRunning
         else:
@@ -70,9 +73,7 @@ class DefaultTrader:
             self._now = pendulum.now().int_timestamp
 
     def _do_analysis(self):
-        if (self.operation.stop_on) and (
-            self.operation.position.side != side.Zeroed
-        ):
+        if (self.operation.stop_on) and (self.operation.position.side != side.Zeroed):
 
             self._step = self._stop_loss_time_frame_in_seconds
             self._stop_analysis()
@@ -87,14 +88,16 @@ class DefaultTrader:
         print("Finally stop alalysis! But, nothing here :/")
 
     def _classifier_analysis(self):
-        if self._now >= (self.operation.last_check.at
-                         + self._classifier_time_frame_in_seconds):
+        if self._now >= (
+            self.operation.last_check.at + self._classifier_time_frame_in_seconds
+        ):
 
             self._by_stop = False
             self._analyze_for(self.Classifier)
 
-            self.logger.results_from = (
-                "CLASSIFIER ({})".format(self.operation.classifier.name))
+            self.logger.results_from = "CLASSIFIER ({})".format(
+                self.operation.classifier.name
+            )
 
             self.operation.last_check.update(by="classifier")
             self.logger.consolidate_log()
@@ -106,34 +109,56 @@ class DefaultTrader:
         self.operation.last_check.update(at=self._now)
         self.KlinesGetter.time_frame = Analyzer.parameters.time_frame
 
-        Analyzer.data_to_analyze = (self.KlinesGetter._get_n_until(
-            number_of_candles=Analyzer.n_samples_to_analyze, until=self._now))
+        Analyzer.data_to_analyze = self.KlinesGetter._get_n_until(
+            number_of_candles=Analyzer.n_samples_to_analyze, until=self._now
+        )
 
         self.logger.last_analyzed_data = Analyzer.data_to_analyze[-1:]
         self.logger.analysis_result = self.analysis_result = Analyzer.result()
         self._signal_interpreter()
         self.OrderHandler.execute(self._signal)
 
+    def _start(self):
+        self.operation.update(status=stat.Running)
+
+        if self.operation.mode == mode.BackTesting:
+            self.operation.last_check.update(at=0)
+            self.operation.position.update(side=side.Zeroed)
+
     def _end(self):
         print("It's the end!")
 
     def _signal_interpreter(self):
-        signal = order_handler.Signal(from_side=self.operation.position.side,
-                                      to_side=self.analysis_result["Hint_side"],
-                                      by_stop=self._by_stop).get()
+        signal = order_handler.Signal(
+            from_side=self.operation.position.side,
+            to_side=self.analysis_result["Hint_side"],
+            by_stop=self._by_stop,
+        ).get()
 
-        self._signal = (sig.Hold
-                        if signal == sig.NakedSell
-                        else sig.Sell if signal == sig.DoubleNakedSell
-                        else sig.Buy if signal == sig.DoubleBuy
-                        else signal)
+        if self.operation.hold_if_stopped and (
+            self.operation.position.due_the_signal
+            in [sig.StoppedFromLong, sig.StoppedFromShort]
+        ):
+
+            self._signal = sig.Hold
+            print("Passing, cause recently stopped!")
+
+        else:
+            self._signal = (
+                sig.Hold
+                if signal == sig.NakedSell
+                else sig.Sell
+                if signal == sig.DoubleNakedSell
+                else sig.Buy
+                if signal == sig.DoubleBuy
+                else signal
+            )
 
         print(" ")
         print("Original_signal: ", signal)
 
     def run(self):
-        self.operation.update(status=stat.Running)
-
+        self._start()
         while self.operation.status == stat.Running:
             self._do_analysis()
             self._get_ready_to_repeat()
