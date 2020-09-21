2c1810d98de516ee9667902d81824cfab16199bf
marcusmello <mandealista@gmail.com>
2020-09-21 19:58:34 -0300

    'Sig' -> 'sig' | An essay on how the information will flow and an outline of the necessary methods to execute the trade order

--- anansi/tradingbot/order_handler.py
+++ anansi/tradingbot/order_handler.py
@@ -1,5 +1,10 @@
-from .models import Trades
-from ..settings import PossibleSides as side, PossibleSignals as Sig
+from . import trade_brokers
+from .models import Wallet, TradesRegister
+from ..settings import (
+    PossibleSides as side,
+    PossibleModes as mode,
+    PossibleSignals as sig,
+)
 
 
 class Signal:
@@ -11,38 +16,59 @@ class Signal:
 
     def get(self):
         if self.from_side == self.to_side:
-            return Sig.Hold
+            return sig.Hold
 
         if self.from_side == side.Zeroed:
             if self.to_side == side.Long:
-                return Sig.Buy
+                return sig.Buy
 
             if self.to_side == side.Short:
-                return Sig.NakedSell
+                return sig.NakedSell
 
         if self.from_side == side.Long:
             if self.to_side == side.Zeroed:
                 if self.by_stop:
-                    return Sig.StoppedFromLong
-                return Sig.Sell
+                    return sig.StoppedFromLong
+                return sig.Sell
 
             if to_side == side.Short:
-                return Sig.DoubleNakedSell
+                return sig.DoubleNakedSell
 
         if self.from_side == side.Short:
             if to_side == side.Zeroed:
                 if self.by_stop:
-                    return Sig.StoppedFromShort
-                return Sig.Buy
+                    return sig.StoppedFromShort
+                return sig.Buy
 
             if to_side == side.Long:
-                return Sig.DoubleBuy
+                return sig.DoubleBuy
 
 
 class OrderHandler:
     def __init__(self, operation):
         self.operation = operation
-        self.trades = Trades(operation=self.operation)
+        self.trades = TradesRegister(operation=self.operation)
+        wallet = Wallet(user=self.operation.user)
+        self.signal = None
+        self.amount = None
+        executor = "_{}_executor".format(self.operation.mode)
+        self._execute = getattr(self, executor)
+        self.broker = getattr(trade_brokers, self.operation.market.exchange)(
+            ticker_symbol=(
+                self.operation.market.quote_asset_symbol
+                + self.operation.market.base_asset_symbol))
+
+    def _price(self):
+        pass
+
+    def _amount(self):
+        factor = self.operation.exposure_factor
+
+        amount = factor*100.0
+
+        self.amount = amount
+
+        return amount
 
     def _save_movement(self):
         pass
@@ -53,9 +79,28 @@ class OrderHandler:
     def _update_wallet(self):
         pass
 
-    def _order_to_broker(self):
+    def _RealTrading_executor(self):
         pass
 
-    def execute(self, signal):
-        print("Interpreted_signal: ", signal)
+    def _RealTimeTest_executor(self):
+        pass
+
+    def _BackTesting_executor(self):
+
+        print("Interpreted_signal: ", self.signal)
         print(" ")
+
+    def execute(self, signal):
+        self.signal = signal
+
+        if self._amount() > self.broker.mininal_amount():
+            self._execute()
+
+            print("Executed because {} > {}".format(
+                self.amount, self.broker.mininal_amount()))
+            print(" ")
+
+        else:
+            print("Fail because {} < {}".format(
+                self.amount, self.broker.mininal_amount()))
+            print(" ")
