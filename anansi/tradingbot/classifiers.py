853f5d4dc83222a91dec9f468b4c144ba48a7c8e
marcusmello <mandealista@gmail.com>
2020-09-26 18:49:59 -0300

    Refactored to return a 'Result' object and update log itself

--- anansi/tradingbot/classifiers.py
+++ anansi/tradingbot/classifiers.py
@@ -1,50 +1,65 @@
+from ..settings import PossibleSides as Side
 import json
-from ..share import tools
-deserialize = tools.Deserialize()
+from ..share.tools import Result, Deserialize
+deserialize = Deserialize()
 
 
 class CrossSMA:
     class DefaultParameters:
         def __init__(self):
-            # TODO: Create classes 'PossiblePriceSource' and
-            # 'PossibleTimeFrames' on settings module
             self.time_frame = "6h"
-            self.price_source = "ohlc4"
+            self.price_metrics = "ohlc4"
             self.smaller_sample = 3
             self.larger_sample = 80
 
-    def __init__(self, parameters, data_to_analyze=None):
+    def __init__(self, parameters, log, data_to_analyze=None):
+        self._indicators_dict = {}
+        self._side = None
+        self._price = None
+        self._SMA_smaller = None
+        self._SMA_larger = None
         self.parameters = deserialize.json2obj(parameters)
+        self.log = log
         self.data_to_analyze = data_to_analyze
         self.n_samples_to_analyze = self.parameters.larger_sample
 
-    def result(self):
-        side = "short"
-        price = (
-            self.data_to_analyze.apply_indicator.price._given(
-                price_source=self.parameters.price_source))
-
-        SMA_smaller = (
-            self.data_to_analyze.apply_indicator.trend.simple_moving_average(
-                number_of_candles=self.parameters.smaller_sample,
-                price_source=self.parameters.price_source))
-
-        SMA_larger = (
-            self.data_to_analyze.apply_indicator.trend.simple_moving_average(
-                number_of_candles=self.parameters.larger_sample,
-                price_source=self.parameters.price_source))
-
-        if SMA_smaller.last() > SMA_larger.last():
-            side = "long"
-
-        return {
-            "Price ({})".format(self.parameters.price_source): price.last(),
+    def _make_indicators_dict(self):
+        self._indicators_dict = {
+            "Price ({})".format(self.parameters.price_metrics):
+            self._price.last(),
 
             "SMA_smaller ({} candles)".format(
-                self.parameters.smaller_sample): SMA_smaller.last(),
+                self.parameters.smaller_sample): self._SMA_smaller.last(),
 
             "SMA_larger ({} candles)".format(
-                self.parameters.larger_sample): SMA_larger.last(),
-
-            "Hint_side": side,
+                self.parameters.larger_sample): self._SMA_larger.last(),
         }
+
+    def _consolidate_log(self):
+        self.log.analyzed_by = "Classifier"
+        self.log.last_analyzed_data = self.data_to_analyze[-1:]
+        self.log.analysis_result = {
+            **self._indicators_dict, "side": self._side}
+
+    def result(self):
+        self._side = Side.Short
+        self._price = self.data_to_analyze.PriceFromKline.using(
+            self.parameters.price_metrics)
+
+        self._SMA_smaller = (
+            self.data_to_analyze.apply_indicator.trend.simple_moving_average(
+                number_of_candles=self.parameters.smaller_sample,
+                metrics=self.parameters.price_metrics))
+
+        self._SMA_larger = (
+            self.data_to_analyze.apply_indicator.trend.simple_moving_average(
+                number_of_candles=self.parameters.larger_sample,
+                metrics=self.parameters.price_metrics))
+
+        if self._SMA_smaller.last() > self._SMA_larger.last():
+            self._side = Side.Long
+
+        self._make_indicators_dict()
+        self._consolidate_log()
+
+        return Result(self._side)
