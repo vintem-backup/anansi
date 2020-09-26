2de7e5a67ecb48f678e84ca12676c752b1a350e7
marcusmello <mandealista@gmail.com>
2020-09-26 18:50:02 -0300

    Some rename | Log is appended on Db by Pony now

--- anansi/tradingbot/models.py
+++ anansi/tradingbot/models.py
@@ -1,10 +1,10 @@
 # import pandas as pd
 import json
+import pendulum
 from pony.orm import *
-from ..settings import Default, Environments
-from ..share.tools import Printers
+from ..settings import *  # Default, Environments
+from ..share.tools import PrintLog
 from ..share.db_handlers import Storage
-from tabulate import tabulate
 
 db, env = Database(), Environments.ENV
 
@@ -22,94 +22,122 @@ class AttributeUpdater(object):
 class User(db.Entity, AttributeUpdater):
     first_name = Required(str, unique=True)
     last_name = Optional(str)
-    login_displayed_name = Optional(str)
     email = Optional(str)
-    operations = Set("Operation", cascade_delete=True)
-    portfolio = Required("Portfolio", cascade_delete=True)
+    operations = Set(lambda: Operation, cascade_delete=True)
+    portfolio = Required(lambda: Portfolio, cascade_delete=True)
+    trades_log = Set(lambda: TradeLog, cascade_delete=False)
+
+    @property
+    def login_displayed_name(self):
+        return "{}_{}".format(self.first_name, self.last_name)
 
 
 class Portfolio(db.Entity, AttributeUpdater):
-    user = Optional("User")
+    user = Optional(User)  # Foreing key
     assets = Optional(Json)
 
 
 class Position(db.Entity, AttributeUpdater):
-    operation = Optional("Operation")
+    operation = Optional(lambda: Operation)  # Foreing key
     side = Required(str, default=Default.side)
-    quote_amount = Optional(float)
+    size_by_quote = Optional(float)
+    due_to_signal = Optional(str)
     exit_reference_price = Optional(float)
-    due_the_signal = Optional(str)
 
 
 class Classifier(db.Entity, AttributeUpdater):
-    operation = Optional("Operation")
+    operation = Optional(lambda: Operation)  # Foreing key
     name = Required(str)
     parameters = Optional(Json)
 
 
 class StopLoss(db.Entity, AttributeUpdater):
-    operation = Optional("Operation")
+    operation = Optional(lambda: Operation)  # Foreing key
     name = Required(str)
     parameters = Optional(Json)
 
 
-class Market(db.Entity):
-    operation = Optional("Operation")
+class Market(db.Entity):  # I's not advise make attributes editable.
+    operation = Optional(lambda: Operation)  # Foreing key
     exchange = Required(str, default=Default.exchange)
     quote_asset_symbol = Required(str, default=Default.quote_asset_symbol)
     base_asset_symbol = Required(str, default=Default.base_asset_symbol)
 
 
 class LastCheck(db.Entity, AttributeUpdater):
-    Operation = Optional("Operation")
-    by_classifier_at = Optional(int)  # timestamp
-    by_stop_loss_at = Optional(int)  # timestamp
+    Operation = Optional(lambda: Operation)  # Foreing key
+    by_classifier_at = Optional(int)  # UTC timestamp
+    by_stop_loss_at = Optional(int)  # UTC timestamp
 
 
 class Operation(db.Entity, AttributeUpdater):
-    user = Required("User")
+    user = Required(User)  # Foreing key
     trader = Required(str, default=Default.trader)
-    position = Required("Position", cascade_delete=True)
-    market = Required("Market", cascade_delete=True)
-    exposure_factor = Required(float, default=1.3)
-    classifier = Required("Classifier", cascade_delete=True)
-    stop_loss = Required("StopLoss", cascade_delete=True)
+    position = Required(Position, cascade_delete=True)
+    market = Required(Market, cascade_delete=True)
+    # Leverage or portfolio proportion:
+    exposure_factor = Required(float, default=1.0)
+    classifier = Required(Classifier, cascade_delete=True)
+    stop_loss = Required(StopLoss, cascade_delete=True)
+    logs = Set(lambda: OperationLog)
 
     status = Required(str, default=Default.status)
     mode = Required(str, default=Default.mode)
     stop_on = Required(bool, default=True)
     hold_if_stopped = Required(bool, default=True)
 
-    last_check = Required("LastCheck")
+    last_check = Required(LastCheck)
 
 
-class Logger(Printers):
-    def __init__(self, operation):
+class OperationLog(db.Entity, AttributeUpdater):
+    operation = Optional(Operation)  # Foreing key
+    claimed_at = Optional(int)  # UTC timestamp
+    analyzed_by = Optional(str)
+    last_analyzed_data = Optional(FloatArray)
+    analysis_result = Optional(Json)
+
+
+class Log(PrintLog):
+    def __init__(self, operation, desired_analyzed_data_information=None):
         self.operation = operation
-        self.storage = Storage(
-            db_name=env._DbPath,
-            table_name="operation_id_{}_log".format(self.operation.id),
-            primary_key="Open_time")
+        self.desired_analyzed_data_information = (
+            kline_desired_informations if not
+            desired_analyzed_data_information
+            else desired_analyzed_data_information)
+    #    self.results_from = None
+    #    self.last_analyzed_data = None
+    #    self.analysis_result = None
+    #    self.results_from = None
 
-        self.last_analyzed_data = None
-        self.analysis_result = None
-        self.results_from = None
-
-    def consolidate_log(self):
+    @db_session
+    def update(self):
         if env.print_log:
             self.print_log()
 
-        self.last_analyzed_data.ParseTime.from_human_readable_to_timestamp()
-        log = self.last_analyzed_data.assign(
-            results=json.dumps(self.analysis_result))
+        self.last_analyzed_data.KlinesDateTime.from_human_readable_to_timestamp()
 
-        self.storage.append_dataframe(log)
+        last_analyzed_data = [float(getattr(
+            self.last_analyzed_data, information).item())
+            for information in
+            self.desired_analyzed_data_information]
 
-    def get_from_db(self, number_of_lines):
-        pass
+        if self.operation.mode == PossibleModes.BackTesting:
 
-    def show(self, number_of_lines):
-        pass
+            self.operation.logs.create(
+                claimed_at=pendulum.now().int_timestamp,
+                analyzed_by=self.analyzed_by,
+                last_analyzed_data=last_analyzed_data,
+                analysis_result=json.dumps(self.analysis_result))
+
+
+class TradeLog(db.Entity, AttributeUpdater):
+    user = Optional(User)  # Foreing key
+    operation_id = Optional(int)
+    timestamp = Optional(int)
+    signal = Optional(str)
+    price = Optional(float)
+    fee = Optional(float)  # base_asset units
+    base_amount = Optional(float)
 
 
 class TradesRegister:
