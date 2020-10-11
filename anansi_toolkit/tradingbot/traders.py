2cc7dfeff1f805fac151e4999409b9e493d4ad19
marcusmello <mandealista@gmail.com>
2020-10-11 20:59:37 -0300

    Refactoring...

--- anansi_toolkit/tradingbot/traders.py
+++ anansi_toolkit/tradingbot/traders.py
@@ -12,15 +12,26 @@ from ..settings import (
     PossibleStatuses as STAT,
 )
 
-
+#!TODO: Testar instanciamento com atributos default/kwargs
 class Order:
-    from_side: str = None
-    to_side: str = None
-    order_type = ORD.Market
-    price: float = None
+    def __init__(
+        self,
+        timestamp:int,
+        from_side: str,
+        to_side: str,
+        price: float,
+        order_type=ORD.Market,
+        by_stop=False,
+    ):
+        self.timestamp = timestamp
+        self.from_side = from_side
+        self.to_side = to_side
+        self.price = price
+        self.order_type = order_type
+        self.by_stop = by_stop
 
 
-class KlinesSimpleTrader:
+class SimpleKlinesTrader:
     def __init__(self, operation):
         self.ticker_symbol = (
             operation.market.quote_asset_symbol
@@ -30,79 +41,39 @@ class KlinesSimpleTrader:
         self.event = tools.EventContainer(reporter=self.__class__.__name__)
         self.log = DefaultLog(operation)
         self.result = None
-        self.order = Order()
-        self._intantiate_klines_getter()
+        self._step = None
         self.OrderHandler = orders.Handler(self.operation, self.log)
         self.Classifier = getattr(classifiers, operation.classifier.name)(
             parameters=operation.classifier.parameters, log=self.log
         )
+        self._instantiate_klines_and_price_getters()
 
-    def _intantiate_klines_getter(self):
-        mode = self.operation.mode
+    def _instantiate_klines_and_price_getters(self):
+        backtesting = bool(self.operation.mode == MODE.BackTesting)
         kwargs = dict(
             broker_name=self.operation.market.exchange,
             ticker_symbol=self.ticker_symbol,
         )
-
+        tf = dict(time_frame=self.Classifier.parameters.time_frame)
         self.KlinesGetter = (
-            handlers.BackTestingKlines(**kwargs)
-            if mode == MODE.BackTesting
-            else handlers.KlinesFromBroker(**kwargs)
+            handlers.BackTestingKlines(**kwargs, **tf)
+            if backtesting
+            else handlers.KlinesFromBroker(**kwargs, **tf)
+        )
+        self.PriceGetter = (
+            handlers.BackTestingPriceGetter(**kwargs)
+            if backtesting
+            else handlers.PriceGetter(**kwargs)
         )
 
     def _get_initial_backtesting_now(self):
-        self.KlinesGetter.time_frame = self.Classifier.parameters.time_frame
-
         step_in_seconds = (
             self.Classifier.step * self.Classifier.number_of_candles
         )
-
         return self.KlinesGetter._oldest_open_time() + step_in_seconds
 
     def _get_final_backtesting_now(self):
-        self.KlinesGetter.time_frame = self.Classifier.parameters.time_frame
         return self.KlinesGetter._newest_open_time()
-
-    def _get_ready_to_repeat(self):
-        if self.operation.mode == MODE.BackTesting:
-            self._now += self.Classifier.step
-
-            if self._now > self._final_backtesting_now:
-                self.operation.status = STAT.NotRunning
-        
-        else:
-            sleep_time = self._step - pendulum.from_timestamp(self._now).second
-            time.sleep(sleep_time)
-            self._now = pendulum.now().int_timestamp
-
-    def _do_analysis(self):
-        self._classifier_analysis()
-
-
-    def _classifier_analysis(self):
-        NewDataToAnalyze = bool(
-            self._now
-            >= (
-                self.operation.last_check.by_classifier_at
-                + self.Classifier.step
-            )
-        )
-
-        if NewDataToAnalyze:
-            self._analyze_for(self.Classifier)
-            self.operation.last_check.update(by_classifier_at=self._now)
-
-        else:
-            print("No new data do analyze")
-
-    def _analyze_for(self, Analyzer):
-        self.KlinesGetter.time_frame = Analyzer.parameters.time_frame
-
-        data = self.KlinesGetter.get(
-            number_of_candles=Analyzer.number_of_candles, until=self._now
-        )
-
-        self.result = Analyzer.get_result_for_this(data)
 
     def _start(self):
         self._now = pendulum.now().int_timestamp
@@ -114,22 +85,83 @@ class KlinesSimpleTrader:
             self._now = self._get_initial_backtesting_now()
             self._final_backtesting_now = self._get_final_backtesting_now()
 
+    def _get_ready_to_repeat(self):
+        if self.operation.mode == MODE.BackTesting:
+            self._now += self._step
+
+            if self._now > self._final_backtesting_now:
+                self.operation.update(status=STAT.NotRunning)
+
+        else:
+            sleep_time = (
+                self._step
+                - pendulum.from_timestamp(self._now).second
+            )
+            time.sleep(sleep_time)
+            self._now = pendulum.now().int_timestamp
+
     def _end(self):  # Not decided what do here yet
         self.event.description = "It's the end!"
         self.log.report(self.event)
 
-    def _prepare_order(self):
-        self.order.price = 9000.0  #! Just a mock
+    def _do_analysis(self):
+        self._step = self.Classifier.step
+        must_check_stop = self.operation.stop_is_on
+        is_positioned = bool(self.operation.position.side != SIDE.Zeroed)
 
+        if is_positioned and must_check_stop:
+            pass
+            # self._step = self.StopLoss.step
+            # self._stop_analysis()
 
+        self._classifier_analysis()
+
+    def _stop_analysis(self):
+        pass
+
+    def _classifier_analysis(self):
+        is_there_a_new_candle = bool(
+            self._now
+            >= (
+                self.operation.last_check.by_classifier_at
+                + self.Classifier.step
+            )
+        )
+
+        if is_there_a_new_candle:
+            kwargs = dict(
+                number_of_candles=self.Classifier.number_of_candles,
+                until=self._now,
+            )
+            data = self.KlinesGetter.get(**kwargs)
+            self.result = self.Classifier.get_result_for_this(data)
+            self.operation.last_check.update(by_classifier_at=self._now)
+
+    def _movement_moderator(self, current_side, suggested_side):
+        return (current_side, suggested_side)
+
+    #!TODO: order to log
+    def _execute_the_order_if_the_side_changes(self):
+        current_side, suggested_side = (
+            self.operation.position.side,
+            self.result.side,
+        )
+        if current_side != suggested_side:
+            timestamp = self._now
+            from_side, to_side = self._movement_moderator(
+                current_side, suggested_side
+            )
+            price = self.PriceGetter.get(at=self._now)
+            by_stop = self.result.by_stop
+            order = Order(timestamp, from_side, to_side, price, by_stop)
+            self.OrderHandler.execute(order)
 
     def run(self):
         self._start()
         while self.operation.status == STAT.Running:
             try:
                 self._do_analysis()
-                self._prepare_order()
-                self.OrderHandler.execute(self.order)
+                self._execute_the_order_if_the_side_changes()
                 self._get_ready_to_repeat()
 
             except Exception as e:
