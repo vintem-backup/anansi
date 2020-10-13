336e26286000615f60975e207510e0a5ab22caab
marcusmello <mandealista@gmail.com>
2020-10-13 19:37:32 -0300

    New '_report_log' method | Order like dict | Fixed useless import

--- anansi_toolkit/tradingbot/traders.py
+++ anansi_toolkit/tradingbot/traders.py
@@ -1,7 +1,7 @@
 import time
 import pendulum
 from ..marketdata import handlers
-from ..share import tools
+from ..share.tools import EventContainer
 from . import classifiers, orders
 from .models import DefaultLog
 
@@ -12,35 +12,15 @@ from ..settings import (
     PossibleStatuses as STAT,
 )
 
-#!TODO: Testar instanciamento com atributos default/kwargs
-class Order:
-    def __init__(
-        self,
-        timestamp:int,
-        from_side: str,
-        to_side: str,
-        price: float,
-        order_type=ORD.Market,
-        by_stop=False,
-    ):
-        self.timestamp = timestamp
-        self.from_side = from_side
-        self.to_side = to_side
-        self.price = price
-        self.order_type = order_type
-        self.by_stop = by_stop
-
-
 class SimpleKlinesTrader:
     def __init__(self, operation):
-        self.ticker_symbol = (
-            operation.market.quote_asset_symbol
-            + operation.market.base_asset_symbol
-        )
+        _quote = operation.market.quote_asset_symbol
+        _base = operation.market.base_asset_symbol
+        self.ticker_symbol = _quote + _base
         self.operation = operation
-        self.event = tools.EventContainer(reporter=self.__class__.__name__)
+        self._event = EventContainer(reporter=self.__class__.__name__)
         self.log = DefaultLog(operation)
-        self.result = None
+        self.last_result = None
         self._step = None
         self.OrderHandler = orders.Handler(self.operation, self.log)
         self.Classifier = getattr(classifiers, operation.classifier.name)(
@@ -81,12 +61,13 @@ class SimpleKlinesTrader:
 
         if self.operation.mode == MODE.BackTesting:
             self.operation.last_check.update(by_classifier_at=0)
-            self.operation.position.update(Side=SIDE.Zeroed)
+            self.operation.position.update(side=SIDE.Zeroed)
             self._now = self._get_initial_backtesting_now()
             self._final_backtesting_now = self._get_final_backtesting_now()
 
     def _get_ready_to_repeat(self):
         if self.operation.mode == MODE.BackTesting:
+            #print_log
             self._now += self._step
 
             if self._now > self._final_backtesting_now:
@@ -101,23 +82,18 @@ class SimpleKlinesTrader:
             self._now = pendulum.now().int_timestamp
 
     def _end(self):  # Not decided what do here yet
-        self.event.description = "It's the end!"
-        self.log.report(self.event)
+        self._report_to_log("It's the end!")
 
     def _do_analysis(self):
         self._step = self.Classifier.step
-        must_check_stop = self.operation.stop_is_on
         is_positioned = bool(self.operation.position.side != SIDE.Zeroed)
 
-        if is_positioned and must_check_stop:
+        if is_positioned and self.operation.is_stop_loss_enabled:
             pass
             # self._step = self.StopLoss.step
             # self._stop_analysis()
 
         self._classifier_analysis()
-
-    def _stop_analysis(self):
-        pass
 
     def _classifier_analysis(self):
         is_there_a_new_candle = bool(
@@ -127,46 +103,49 @@ class SimpleKlinesTrader:
                 + self.Classifier.step
             )
         )
-
         if is_there_a_new_candle:
             kwargs = dict(
                 number_of_candles=self.Classifier.number_of_candles,
                 until=self._now,
             )
             data = self.KlinesGetter.get(**kwargs)
-            self.result = self.Classifier.get_result_for_this(data)
+            self.last_result = self.Classifier.get_result_for_this(data)
             self.operation.last_check.update(by_classifier_at=self._now)
+    def _stop_analysis(self):
+        pass
 
-    def _movement_moderator(self, current_side, suggested_side):
-        return (current_side, suggested_side)
-
-    #!TODO: order to log
     def _execute_the_order_if_the_side_changes(self):
-        current_side, suggested_side = (
-            self.operation.position.side,
-            self.result.side,
-        )
+        current_side = self.operation.position.side
+        suggested_side = self.last_result.side
+        
         if current_side != suggested_side:
-            timestamp = self._now
-            from_side, to_side = self._movement_moderator(
-                current_side, suggested_side
+            order = dict(
+                timestamp=self._now,
+                from_side=current_side,
+                to_side=suggested_side, 
+                price = self.PriceGetter.get(at=self._now),
+                due_to_stop = self.last_result.due_to_stop,
             )
-            price = self.PriceGetter.get(at=self._now)
-            by_stop = self.result.by_stop
-            order = Order(timestamp, from_side, to_side, price, by_stop)
             self.OrderHandler.execute(order)
+
+    def _report_to_log(self, event_description:str):
+        self._event.description = event_description
+        self.log.report(self._event)
+
+    def print_log(self):
+        pass
 
     def run(self):
         self._start()
         while self.operation.status == STAT.Running:
-            try:
-                self._do_analysis()
-                self._execute_the_order_if_the_side_changes()
-                self._get_ready_to_repeat()
+            #try:
+            self._do_analysis()
+            self._execute_the_order_if_the_side_changes()
+            self._get_ready_to_repeat()
 
-            except Exception as e:
-                self.event.description = str(e)
-                self.log.report(self.event)
-
-            self.log.update()
+            #except Exception as e:
+                #self._report_to_log(str(e))
+            
+            self.log.update(timestamp=self._now)
+        
         self._end()
